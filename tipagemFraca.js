d85cb0de43fcb9205a2a464ade53bf1807ec896d
Daniel Moura <daniel.mlds@gmail.com>
2020-10-17 12:08:35 -0300

    Fundamentos Javascript

---
+++ tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = "legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1443
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evite usar nomes genéricos para variáveis e constantes
+let valor = ""
+let numero = 1
+let pqp = false //produto quimico perigoso kkk...
