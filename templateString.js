11d82f366600678b42820e9d19d67d151aad562e
Daniel Moura <daniel.mlds@gmail.com>
2020-10-18 14:22:04 -0300

    String fundamentos

---
+++ templateString.js
@@ -0,0 +1,9 @@
+const nome = "Marie"
+const concatenacao = "Olá " + nome + "!"
+console.log(concatenacao)
+
+//expressoes...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei..${up('cuidado')}!`)
