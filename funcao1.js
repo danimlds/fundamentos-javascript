409d4d7db9a414615787a9b883e699062ca94674
Daniel Moura <daniel.mlds@gmail.com>
2021-01-19 14:33:03 -0300

    Function

---
+++ funcao1.js
@@ -0,0 +1,17 @@
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(2, 10, 3, 2, 2)
+imprimirSoma()
+
+
+function soma(a, b = 0) {
+    return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
+console.log(soma())
