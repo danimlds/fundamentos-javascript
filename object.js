a6c23a290d3c4ba4440ef3503371f6ab09de2ada
Daniel Moura <daniel.mlds@gmail.com>
2021-01-19 12:56:02 -0300

    Entendendo Object

---
+++ object.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = "Celular"
+prod1.preco = 4000.00
+prod1['Desconto Legal'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.00
+}
+
+console.log(prod2)
