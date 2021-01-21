8237c4dccb1bacce83eedc27b0c382729d632124
Daniel Moura <daniel.mlds@gmail.com>
2021-01-21 17:53:35 -0300

    Operadores

---
+++ parNomeValor.js
@@ -0,0 +1,20 @@
+const saudacao = 'Opa'
+
+function exec() {
+    const saudacao = 'Fala'
+    return saudacao
+}
+
+const clinte = {
+    nome: 'Jason',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Irara',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(clinte)
